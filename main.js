ea177ae09b71b12d64421eee160bf20231c51fe3
madhavparekh <madhavparekh@gmail.com>
2018-02-22 21:38:01 -0800

    updated URL - it works

--- main.js
+++ main.js
@@ -7,28 +7,32 @@ var queryObj = {
 }
 
 $(document).ready(function(){
-    
+    var url = '';
+
     $('#searchBtn').on('click', function(r){
         queryObj.q = $('#term').val();
-        console.log(queryObj.q);
+        
         queryObj.page = $('#pages').val();
         queryObj.begin_date = $('#startYear').val();
         queryObj.end_date = $('#endYear').val();
+        console.log(queryObj);
+        
+        url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param(queryObj);
+
+        console.log(url);
+        getArticles();
     });
     
-    var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json";
-    url += '?' + $.param(queryObj);
-
-    console.log(url);
-
-
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(result) {
-        console.log(result);
-    }).fail(function(err) {
-        throw err;
-    });
+    function getArticles(){
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(result) {
+            console.log(result);
+        }).fail(function(err) {
+            throw err;
+        });
+    }
 
 });
