f6aad8db372eb601c0ab850a535e361175795cae
madhavparekh <madhavparekh@gmail.com>
2018-02-23 07:09:05 -0800

    made Dates optional. Dates beign added to obj only if entered in form

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var queryObj = {
     'api-key' : "db88f4c617ed41e1a9b08bcb955573e4",
     'q' : '',
     'page' : 0,
-    'begin_date': '20180101',
-    'end_date' : '20180228',
+    // 'begin_date': '20180101',
+    // 'end_date' : '20180228',
 }
 
 $(document).ready(function(){
@@ -12,10 +12,13 @@ $(document).ready(function(){
     $('#searchBtn').on('click', function(r){
         queryObj.q = $('#term').val();
         
-        queryObj.page = $('#pages').val();
-        queryObj.begin_date = $('#startYear').val();
-        queryObj.end_date = $('#endYear').val();
-        console.log(queryObj);
+        queryObj.page = parseInt($('#pages').val());
+        if($('#startYear').val() !== '')
+            queryObj['begin_date'] = $('#startYear').val(); 
+        if($('#endYear').val() !== '')
+            queryObj['end_date'] = $('#endYear').val();
+        
+            console.log(queryObj);
         
         url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param(queryObj);
@@ -30,6 +33,15 @@ $(document).ready(function(){
             method: 'GET',
         }).done(function(result) {
             console.log(result);
+
+            var articles = result.response.docs;
+
+            articles.forEach(e => {
+                var htmlDisplay = $('<div>');
+                htmlDisplay.append('<h4>').html(e.headline.main, '<br>');
+                $('#articles').append(htmlDisplay);
+            });
+
         }).fail(function(err) {
             throw err;
         });
